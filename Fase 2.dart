2856edad6949ef5a665c234d563a3464e82e50a2
Marina Villaça <62123518+marinavillaca@users.noreply.github.com>
2025-01-14 18:04:58 -0300

    Fase 2.dart

---
+++ Fase 2.dart
@@ -0,0 +1,10 @@
+void main() {
+  print(
+      'Sejam bem-vindos a loja Cuidapet!\nEm breve teremos um sistema de autoatendimento.');
+
+  String enderecoLoja = 'Avenida Brasil';
+  int numero = 830;
+
+  print('Endereço: $enderecoLoja');
+  print('Número: $numero');
+}
