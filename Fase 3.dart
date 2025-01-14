1896b97d4ea990857a1d555497e6b5e017902dfa
Marina Villaça <62123518+marinavillaca@users.noreply.github.com>
2025-01-14 18:17:12 -0300

    Fase 3.dart

--- Fase 3.dart
+++ Fase 3.dart
@@ -5,6 +5,6 @@ void main() {
   var nomeCliente = stdin.readLineSync() as String;
 
   print('Prezado(a), $nomeCliente. Seja muito bem-vindo(a) à nossa loja.');
-  print(
-      'Oferecemos em nossa loja produtos e serviços para seu PET. Para venda de produtos, procure o colaborador Junior e, para serviços como banho ou tosa, procure o colaborador Neto. Obrigado e esperamos que tenha uma ótima experiência em nossa loja.');
+  print('Oferecemos em nossa loja produtos e serviços para seu PET. Para venda de produtos, procure o colaborador Junior e, para serviços como banho ou tosa, procure o colaborador Neto. Obrigado e esperamos que tenha uma ótima experiência em nossa loja.');
+
 }
